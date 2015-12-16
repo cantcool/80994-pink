32077563a5991b2f1d937940f216ba0b753c0ba0
cantcool <vyachaslau@gmail.com>
2015-12-16 20:48:55 +0300

    Media queries form.html styling

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,10 +27,10 @@ module.exports = function(grunt) {
 
     watch: {
       style: {
-        files: ["sass/**/*.scss"],
+        files: ["sass/**/*.scss", "**/*.html"],
         tasks: ["sass", "postcss"],
         options: {
-          spawn: false,
+          spawn: true,
           livereload: true
         }
       }
