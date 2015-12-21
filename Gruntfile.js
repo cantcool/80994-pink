3cb9709fc0177bae06341becbc443ce05453a50b
cantcool <vyachaslau@gmail.com>
2015-12-21 18:25:34 +0300

    photo delete-btn hover styling

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function(grunt) {
 
     watch: {
       style: {
-        files: ["sass/**/*.scss", "**/*.html"],
+        files: ["sass/**/*.scss"],
         tasks: ["sass", "postcss"],
         options: {
           spawn: true,
